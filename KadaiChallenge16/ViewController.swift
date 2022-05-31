9f18ca28c20a82cbaa4baca6175f340ff4175be9
Sena <senana0116@icloud.com>
2022-05-31 20:18:54 +0900

    first commit

---
+++ KadaiChallenge16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KadaiChallenge16
+//
+//  Created by 澤田世那 on 2022/05/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
